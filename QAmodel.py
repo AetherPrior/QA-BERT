e66eb553397f042014d763d676b7cf4559e455dd
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2021-03-23 02:48:17 +0530

    Finish class definition

--- QAmodel.py
+++ QAmodel.py
@@ -1,16 +1,43 @@
 from config import *
+import os
+
+# File conversion
+from haystack.file_converter.pdf import PDFToTextConverter
+
+# Document Stores
+from haystack.document_store.memory import InMemoryDocumentStore
+from haystack.document_store.faiss import FAISSDocumentStore
+
+# Preprocessing from haystack's side
+from haystack.preprocessor.preprocessor import PreProcessor
+
+# Dense passage retrieval for answering
+from haystack.retriever.dense import DensePassageRetriever
+from haystack.reader.farm import FARMReader
+
+from haystack.utils import print_answers
+
+# Different pipelines for each model
+from haystack.pipeline import ExtractiveQAPipeline, DocumentSearchPipeline, GenerativeQAPipeline
 
 
 class QAmodel:
-    def __init__(self, mode, docs):
-        self._docs = docs
-        self.model = None
+    def __init__(self, base_dir, exp=False, speed=True, gpu=False):
+        # data
+        self._base_dir = base_dir
+
+        # model (built?)
+        self._model = False
+
+        # components of the pipeline
         self._converter = None
         self._doc_store = None
         self._processor = None
         self._retriever = None
         self._reader = None
-        self._mode = mode
+
+        # modes
+        self._exp, self._speed, self._gpu = exp, speed, gpu
         self._pipeline = None
 
     def execute_query(self, query):
@@ -20,22 +47,59 @@ class QAmodel:
         postcondition: The answer(s) is(are) generated and returned
         '''
 
-        if (self.model is None):
+        if (not self._model):
             print(
                 "A model has not been built! We can build it if you wish: [y/N]")
             opt = input()
             if (opt.lower() == 'y'):
                 self.build_model()
+                self.add_pipeline()
             else:
                 return
 
-        prediction = pipe.run(query="What is a parser?",
-                              top_k_retriever=5, top_k_reader=5)
+        elif(self._pipeline is None):
+            print(
+                "A pipeline has not been attached! We can build it if you wish: [y/N]")
+            opt = input()
+            if (opt.lower() == 'y'):
+                self.build_model()
+                self.add_pipeline()
+            else:
+                return
+
+        prediction = self._pipeline.run(query=query,
+                                        top_k_retriever=5, top_k_reader=5)
+
+        return prediction['answers']
+
+    def convert_data(self, base_dir):
+        '''
+        Enter PDF here
+        PDFs normally have to be cleaned, but unfortunately
+        due to the large variety in text, this process is not automated.
+        Recommendation: Strip the title, index and references
+        '''
+        textbooks = [subj for subj in os.listdir(base_dir)
+                     if os.path.isfile(
+            os.path.join(base_dir, subj))]
+
+        print("Converting all of these textbooks :", textbooks)
+
+        converter = [PDFToTextConverter(remove_numeric_tables=False)
+                     for i in textbooks]
+
+        documents = [converter[i].convert(file_path=os.path.join(
+            base_dir, textbooks[i])) for i in range(len(textbooks))]
+
+        return documents
 
     def build_model(self):
-
+        '''
+        Automatic model builder, given the mode of operation
+        Different retrievers, readers and pipelines can be supported as well
+        '''
         # Experimental vs Actual
-        if (self._mode & EXP == 1):
+        if (self._exp):
             self._doc_store = InMemoryDocumentStore(similarity="dot_product")
         else:
             self._doc_store = FAISSDocumentStore(similarity="dot_product")
@@ -44,35 +108,48 @@ class QAmodel:
             clean_empty_lines=True,
             clean_whitespace=True,
             clean_header_footer=True,
-            split_by='passage',
+            split_by='word',
             split_overlap=10,
-            split_respect_sentence_boundary=False
+            # Does not work while splitting by a passage
+            split_respect_sentence_boundary=True
         )
 
-        doc_p = processor.process(doc)
+        documents = self.convert_data(self._base_dir)
+
+        doc_p = []
+        for doc in documents:
+            doc_p += self._processor.process(doc)
+
+        print(type(doc_p[0]))
+
         self._doc_store.write_documents(doc_p)
 
         self._retriever = DensePassageRetriever(
-            document_store=doc_store,
+            document_store=self._doc_store,
             query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
             passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base"
         )
 
-        self._doc_store.update_embeddings(retriever)
+        self._doc_store.update_embeddings(self._retriever)
 
         # for poor guys like me who don't have a gpu
-        gpu_yn = True if self._mode & GPU == 1 else False
+        gpu_yn = True if self._gpu == 1 else False
 
-        if (self._mode & SPEED == 1):
+        if (self._speed):
             # prefer speed over acc, people are impatient
             self._reader = FARMReader(
                 model_name_or_path="deepset/minilm-uncased-squad2", use_gpu=gpu_yn)
         else:
-            # prefer acc over spped, someone who likes to wait
+            # prefer acc over speed, someone who likes to wait
             self._reader = FARMReader(
                 model_name_or_path="deepset/minilm-uncased-squad2", use_gpu=gpu_yn)
 
     def add_pipeline(self, pipeline='default'):
-        if (pipeline == 'default'):
+        if (pipeline == 'default' or pipeline == 'read'):
             self._pipeline = ExtractiveQAPipeline(
                 self._reader, self._retriever)
+
+        elif (pipeline == 'retrive'):
+            self._pipeline = DocumentSearchPipeline(
+                self._retriever
+            )
--- README.md
+++ README.md
@@ -1,15 +1,12 @@
----
-title: QA-BERT
-created: '2021-03-21T19:52:22.795Z'
-modified: '2021-03-21T21:20:35.752Z'
----
-
 # QA-BERT
+
 A Question-Answering model based off of HuggingFace's transformer models, using the [haystack](!https://github.com/deepset-ai/haystack) library.  
-This is a rewrite and improvement upon the original code written using [cdqa](!https://github.com/cdqa-suite/cdQA), a now outdated library. 
+This is a rewrite and improvement upon the original code written using [cdqa](!https://github.com/cdqa-suite/cdQA), a now outdated library.
 
 # Setup
+
 Clone the repository:
+
 ```
 git clone git@github.com:AetherPrior/qa-bert-haystack.git
 OR
@@ -17,21 +14,26 @@ git clone https://github.com/AetherPrior/qa-bert-haystack.git
 ```
 
 Create a new `virtualenv`
+
 ```
 python -m venv /my/venv
 source /my/venv/bin/activate
 ```
+
 Install the required libraries
+
 ```
 pip install -r requirements.txt
 ```
+
 **NOTE:** There is no support for `python3.9` as of yet from `haystack`. Recommend against using any version `>=python3.8` for running the code
 
 # Running
-run ```python main.py```
+
+run `python main.py`
 
 # Contributing
+
 (tbd CONTRIBUTING.md)
 
 Additionally, the docs for `haystack` are quite terse, and it appears to not be a well-known library. A lot of breaking changes happen over a very small time-frame, leaving many questions on their issue-tracker no longer valid. Recommend downloading the source code with the release that you're using to resolve most errors.
-
